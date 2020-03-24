66df27d3e6c5ee18f963e7a5e7eda354de68033e
Mauricio Peña Gonzalez <losarchivosmauricio@gmail.com>
2020-03-23 21:16:15 -0600

    sdas

--- multiple.sequences.py
+++ multiple.sequences.py
@@ -6,5 +6,5 @@ ages = [25, 30, 31, 39]
 for position in range(len(people)):
     person = people[position]
     age = ages[position]
-# comentario para hacer un test :)
+# hacer un test :)
 print(person, age)
